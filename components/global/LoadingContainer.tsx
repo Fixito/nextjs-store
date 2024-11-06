76ff5789ace71ee1aa52d1c0070d139281eed4cf
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-11-06 10:34:15 +0100

    feat(home): Add loading state for Featured Products with a new LoadingContainer

--- components/global/LoadingContainer.tsx
+++ components/global/LoadingContainer.tsx
@@ -1,3 +1,24 @@
+import { Card, CardContent } from '../ui/card';
+import { Skeleton } from '../ui/skeleton';
+
 export default function LoadingContainer() {
-  return <div>LoadingContainer</div>;
+  return (
+    <div className="grid gap-4 pt-12 md:grid-cols-2 lg:grid-cols-3">
+      <LoadingProduct />
+      <LoadingProduct />
+      <LoadingProduct />
+    </div>
+  );
+}
+
+function LoadingProduct() {
+  return (
+    <Card>
+      <CardContent className="p-4">
+        <Skeleton className="h-48 w-full" />
+        <Skeleton className="mt-4 h-4 w-3/4" />
+        <Skeleton className="mt-4 h-4 w-1/2" />
+      </CardContent>
+    </Card>
+  );
 }
