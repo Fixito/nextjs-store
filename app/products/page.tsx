85c1095f15a6b6074bc9db32c306d4473b91f8f9
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-11-06 13:09:26 +0100

    feat(products): Update ProductsPage to handle async searchParams and improve parameter resolution

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,15 +1,16 @@
-import { use } from 'react';
-
 import ProductsContainer from '@/components/products/ProductsContainer';
 
 type SearchParams = { layout?: string; search?: string };
 
-export default function ProductsPage(props: { searchParams: SearchParams }) {
-  // @ts-expect-error - This is a hack to get around the fact that `use` is not typed
-  const searchParams: SearchParams = use(props.searchParams);
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) {
+  const resolvedSearchParams = await searchParams;
 
-  const layout = searchParams.layout || 'grid';
-  const search = searchParams.search || '';
+  const layout = resolvedSearchParams.layout || 'grid';
+  const search = resolvedSearchParams.search || '';
 
   return <ProductsContainer layout={layout} search={search} />;
 }
