432a928568f3035a50fca27d129eec94a4b00854
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-11-06 11:50:54 +0100

    feat(search): Implement search functionality

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import Container from '../global/Container';
 import CartButton from './CartButton';
 import DarkMode from './DarkMode';
@@ -10,7 +12,9 @@ export default function Navbar() {
     <nav className="border-b">
       <Container className="flex flex-col flex-wrap gap-4 py-8 sm:flex-row sm:items-center sm:justify-between">
         <Logo />
-        <NavSearch />
+        <Suspense>
+          <NavSearch />
+        </Suspense>
         <div className="flex items-center gap-4">
           <CartButton />
           <DarkMode />
